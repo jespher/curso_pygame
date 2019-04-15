021105cd51fb862197cfba468e531dbc61900d51
jdrasini <luis.88.rasini@gmail.com>
2019-04-14 22:49:46 -0300

    continuacao do jogo

--- jogo.py
+++ jogo.py
@@ -4,22 +4,21 @@ import pygame
 def main():
     # As definicoes dos obejtos(variaveis)
     pygame.init()
-    tela = pygame.display.set_mode([350, 350])
-    pygame.display.set_caption("Iniciando com Pygame")
+    tela = pygame.display.set_mode([600, 450])
+    pygame.display.set_caption("Jogo Iniciante")
     relogio = pygame.time.Clock()
     cor_branca = (255, 255, 255)
     cor_azul = (108, 194, 236)
     cor_verde = (152, 231, 114)
     cor_vermelha = (237, 28, 36)
     cor_amarela = (219, 235, 18)
-    superficie = pygame.Surface((200, 200))
+    superficie = pygame.Surface((600, 450))
     superficie.fill(cor_azul)
 
-    superficie2 = pygame.Surface((100, 100))
-    superficie2.fill(cor_verde)
-
-    ret = pygame.Rect(10, 10, 45, 45)
-    ret2 = pygame.Rect(50, 50, 100, 50)
+    ret = pygame.Rect(10, 10, 30, 30)
+    ret2 = pygame.Rect(10, 40, 555, 7)
+    ret3 = pygame.Rect(10, 90, 350, 7)
+    ret4 = pygame.Rect(410, 90, 225, 7)
     sair = False
 
     pygame.font.init()
@@ -36,48 +35,35 @@ def main():
                 sair = True
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pygame.mouse.set_pos(150, 150)
-
-            # if event.type == pygame.MOUSEMOTION:
-            #     ret = ret.move(-10, -10)
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    ret.move_ip(-10, 0)
-
-                if event.key == pygame.K_RIGHT:
-                    ret.move_ip(10, 0)
-
-                if event.key == pygame.K_UP:
-                    ret.move_ip(0, -10)
-
-                if event.key == pygame.K_DOWN:
-                    ret.move_ip(0, 10)
-
-                if event.key == pygame.K_SPACE:
-                    ret.move_ip(10, 10)
-
-                if event.key == pygame.K_BACKSPACE:
-                    ret.move_ip(-10, -10)
+                pygame.mouse.set_pos(10, 10)
+                main()
 
         relogio.tick(50)
         tela.fill(cor_branca)
-        tela.blit(superficie, [50, 50])
-        tela.blit(superficie2, [50, 50])
+        tela.blit(superficie, [0, 0])
 
         (xant, yant) = (ret.left, ret.top)
         (ret.left, ret.top) = pygame.mouse.get_pos()
         ret.left -= ret.width / 2
         ret.top -= ret.height / 2
-        if ret.colliderect(ret2):
-            text = fonte_perdeu.render('COLIDIU!!', 1, (255, 255, 255))
+        if ret.colliderect(ret2) or ret.colliderect(ret3):
+            text = fonte_perdeu.render('VOCÊ PERDEU!!', 1, (255, 255, 255))
+            tela.blit(text, (210, 210))
+            pygame.mouse.set_pos(10, 10)
             audio_explosao.play()
             audio_explosao.set_volume(00.1)
-            tela.blit(text, (150, 150))
             (ret.left, ret.top) = (xant, yant)
+
+        if ret.top > 400:
+            text = fonte_ganhou.render('VOCÊ GANHOU!!', 1, (255, 255, 255))
+            tela.blit(text, (210, 210))
+            text = fonte_perdeu.render('Clique para recomeçar!!', 1, cor_vermelha)
+            tela.blit(text, (150, 250))
 
         pygame.draw.rect(tela, cor_vermelha, ret)
         pygame.draw.rect(tela, cor_amarela, ret2)
+        pygame.draw.rect(tela, cor_amarela, ret3)
+        pygame.draw.rect(tela, cor_amarela, ret4)
         pygame.display.update()
     pygame.quit()
 
